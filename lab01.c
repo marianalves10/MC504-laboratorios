e480acb5ea6cf30f174289922b9e24981eb9ea46
Mariana <mariana.sousa@neuralmind.ai>
2022-04-10 16:42:03 -0300

    fix: fix check square

--- lab01.c
+++ lab01.c
@@ -19,15 +19,15 @@ int main(void)
 {
 
     int tabuleiro[9][9] = {  
-            {6, 2, 4, 5, 3, 9, 1, 8, 7},
-            {5, 1, 9, 7, 2, 8, 6, 3, 4},
-            {8, 3, 7, 6, 1, 4, 2, 9, 5},
-            {1, 4, 3, 8, 6, 5, 7, 2, 9},
-            {9, 5, 8, 2, 4, 7, 3, 6, 1},
-            {7, 6, 2, 3, 9, 1, 4, 5, 8},
-            {3, 7, 1, 9, 5, 6, 8, 4, 2},
-            {4, 9, 6, 1, 8, 2, 5, 7, 3},
-            {2, 8, 5, 4, 7, 3, 9, 1, 6}
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}
         };
     
     auxiliar **vetor = malloc(sizeof(auxiliar *)*10);
@@ -136,15 +136,15 @@ int main(void)
 // retorna 0 em caso de algum número repetido ou faltate, 1 caso contrário 
 void* check_quadrado(void* parametros) {
     auxiliar* quadrado = (auxiliar*) parametros;
-    int contador[9] = {0};
+    int contador[10] = {1};
     int i,j;
 
     // percorre todas as linhas do quadrado
-    for (i = quadrado->linha; i < 3 + quadrado->linha; i ++) {
+    for (i = quadrado->linha; i < 3 + quadrado->linha; ++i) {
         // percorre todas as colunas de uma linha
-        for (j = quadrado->coluna; j < 3 + quadrado->coluna; j++) {
+        for (j = quadrado->coluna; j < 3 + quadrado->coluna; ++j) {
             // contador é atualizado com contagem de cada número até o momento
-            contador[quadrado->tabuleiro[i][j] - 1] += 1;
+            contador[quadrado->tabuleiro[i][j]] += 1;
         }
     }
     // se, ao final, o contador tiver algum número != 1, retornar 0
